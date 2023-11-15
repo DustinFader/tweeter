0ca34f0a67a22ba4cef2d50bfd98e195d9271f6e
DustinFader <dustinfader16@gmail.com>
2023-11-15 19:35:11 -0400

    add timeago library for use in the tweets of how long ago they were created

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,39 +1,38 @@
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "content": {
-      "text": "Je pense , donc je suis"
+    content: {
+      text: "Je pense , donc je suis",
     },
-    "created_at": 1461113959088
-  }
-]
+    created_at: 1461113959088,
+  },
+];
 
 const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  tweets.forEach(tweet => {
+  tweets.forEach((tweet) => {
     const $tweet = createTweetElement(tweet);
     console.log($tweet); // Check the structure and content of the created tweet
-    $('#tweets-container').append($tweet); // Append the tweet to the container
-  })
-}
+    $("#tweets-container").append($tweet); // Append the tweet to the container
+  });
+};
 
 const createTweetElement = function (tweetData) {
   const $tweet = $(`
@@ -48,7 +47,7 @@ const createTweetElement = function (tweetData) {
   <h3>${tweetData.content.text}</h3>
   <hr>
   <div>
-  <p>${new Date(tweetData.created_at)}</p>
+  <p>${timeago.format(new Date(tweetData.created_at))}</p>
   <div>
   <i class="fa-solid fa-flag"></i>
   <i class="fa-solid fa-retweet"></i>
@@ -61,11 +60,11 @@ const createTweetElement = function (tweetData) {
 };
 
 $(document).ready(function () {
-  renderTweets(data);
-  $('.tweet-button').on('submit', function(event) {
+  $(".tweet-button").on("submit", function (event) {
     event.preventDefault();
-    console.log($(this).serialize());
-    $.post('/tweets', $(this).serialize());
-  })
+    $.post("/tweets", $(this).serialize());
+    $.get("./tweets", { method: "GET" }).then(function (data) {
+      renderTweets(data);
+    });
+  });
 });
-
