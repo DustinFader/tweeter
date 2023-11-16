57f11c08fee804c3ea0ea4d1a243c7312cd6dc02
DustinFader <dustinfader16@gmail.com>
2023-11-15 21:13:54 -0400

    add some alert catches for various empty values being passed into the tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,9 @@
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   tweets.forEach((tweet) => {
     const $tweet = createTweetElement(tweet);
-    console.log($tweet); // Check the structure and content of the created tweet
     $("#tweets-container").append($tweet); // Append the tweet to the container
   });
 };
@@ -62,8 +36,20 @@ const createTweetElement = function (tweetData) {
 $(document).ready(function () {
   $(".tweet-button").on("submit", function (event) {
     event.preventDefault();
+    const text = $(this).find("textarea").val();
+    
+    if (!text) {
+      alert("Empty tweet");
+      return;
+    }
+
+    if (text > 140) {
+      alert("Tweet too long")
+      return;
+    }
+
     $.post("/tweets", $(this).serialize());
-    $.get("./tweets", { method: "GET" }).then(function (data) {
+    $.get("/tweets", { method: "GET" }).then(function (data) {
       renderTweets(data);
     });
   });
